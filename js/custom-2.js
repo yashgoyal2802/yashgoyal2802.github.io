032abae185f2cecc34a70ff4e838f6009cf0e59a
Yash Goyal <yashdineshgoyal@gmail.com>
2024-12-29 14:29:02 +0000

    Updating the new custom code

--- js/custom-2.js
+++ js/custom-2.js
@@ -1,5 +1,64 @@
 jQuery(document).ready(function () {
     "use strict";
+    (function ($, win) {
+        $.fn.inViewport = function (cb) {
+          return this.each(function (i, el) {
+            function visPx() {
+              var elH = $(el).outerHeight(),
+                H = $(win).height(),
+                r = el.getBoundingClientRect(),
+                t = r.top,
+                b = r.bottom;
+              return cb.call(
+                el,
+                Math.max(0, t > 0 ? Math.min(elH, H - t) : Math.min(b, H))
+              );
+            }
+            visPx();
+            $(win).on("resize scroll", visPx);
+          });
+        };
+      })(jQuery, window);
+    
+      function sleep(ms) {
+        // console.log('sleeping');
+        return new Promise((resolve) => setTimeout(resolve, ms));
+      }
+      async function change_heading_text() {
+        await sleep(2500);
+        $("#heading-text").fadeOut();
+        await sleep(500);
+        $("#heading-text").html("<span class='line'>I am Yash.</span>");
+        await sleep(500);
+        $("#heading-text").fadeIn();
+      }
+    
+      change_heading_text();
+    
+      var scene = document.getElementById("scene");
+      var parallaxInstance = new Parallax(scene);
+    
+      const s = window.screen;
+      const w = (q.width = s.width);
+      const h = (q.height = s.height);
+    
+      const ctx = q.getContext("2d");
+    
+      const p = Array(Math.floor(w / 10) + 1).fill(0);
+    
+      const random = (items) => items[Math.floor(Math.random() * items.length)];
+    
+      const hex = "תשקצץיףסואןבגדהזחטךכלםמנעפר".split("");
+    
+      setInterval(() => {
+        ctx.fillStyle = "rgba(0,0,0,.1)";
+        ctx.fillRect(0, 0, w, h);
+        ctx.fillStyle = "#22eaaa";
+        p.map((v, i) => {
+          ctx.fillText(random(hex), i * 10, v);
+          p[i] = v >= h || v > 50 + 1000 * Math.random() ? 0 : v + 10;
+        });
+      }, 40);
   
     // Utility to calculate polygon vertices
     function getXY(i, radius, sides, radOffset, width, height) {
@@ -104,22 +163,5 @@ jQuery(document).ready(function () {
       $(".probootstrap-mobile-menu-active").removeClass("show");
       $(".probootstrap-burger-menu.visible-xs").removeClass("active");
     });
-  
-    // Sleep function
-    function sleep(ms) {
-      return new Promise((resolve) => setTimeout(resolve, ms));
-    }
-  
-    // Change heading text with animation
-    async function change_heading_text() {
-      await sleep(2500);
-      $("#heading-text").fadeOut();
-      await sleep(500);
-      $("#heading-text").html("<span class='line'>I am Yash.</span>");
-      await sleep(500);
-      $("#heading-text").fadeIn();
-    }
-  
-    change_heading_text();
   });
   
